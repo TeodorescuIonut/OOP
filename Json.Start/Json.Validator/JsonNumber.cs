51e93339f1872faf201dc675cc1f45fb52149c4d
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-01-24 13:31:57 +0200

    refactor some functions

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -7,8 +7,58 @@ namespace Json
         public static bool IsJsonNumber(string input)
         {
             return CheckIfNullorEmpty(input)
-                && CheckIfIsADigit(input)
-                && !CanHaveLeadingZero(input);
+                && CheckAllowedFormat(input)
+                && CheckIfOneOrMoreDigits(input)
+                && CheckTypeOfNumber(input);
+        }
+
+        private static bool CheckTypeOfNumber(string input)
+        {
+                return CanBeAFraction(input) || CanBeAWholeNumber(input);
+        }
+
+        private static bool CanBeAWholeNumber(string input)
+        {
+            return !CanHaveLeadingZero(input) && !CanBeAFraction(input);
+        }
+
+        private static bool CanBeAFraction(string input)
+        {
+            if (!input.Contains('.'))
+            {
+                return false;
+            }
+
+            return HaveLeadingZeroAsAFraction(input) || !HaveLeadingZeroAsAFraction(input);
+        }
+
+        private static bool CheckIfOneOrMoreDigits(string input)
+        {
+            return CanHaveOneDigit(input) || CanHaveMultipleDigits(input);
+        }
+
+        private static bool CanHaveMultipleDigits(string input)
+        {
+            return !CanHaveOneDigit(input);
+        }
+
+        private static bool CanHaveOneDigit(string input)
+        {
+            const int twoDigit = 2;
+            return input.Length < twoDigit;
+        }
+
+        private static bool HaveLeadingZeroAsAFraction(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input.StartsWith('0') && input[i + 1] == '.')
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         private static bool CanHaveLeadingZero(string input)
@@ -22,11 +72,11 @@ namespace Json
             return !string.IsNullOrEmpty(input);
         }
 
-        private static bool CheckIfIsADigit(string input)
+        private static bool CheckAllowedFormat(string input)
         {
             foreach (char c in input)
             {
-                if (!char.IsDigit(c) && c != '.' && c != '-')
+                if (!char.IsDigit(c) && c != '-' && c != '.')
                 {
                     return false;
                 }
