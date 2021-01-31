41d1139dc95c3789a07bc0c7e472b681b4a3052d
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-01-31 22:41:05 +0200

    Created Football ranking project and added Team.cs and TeamFacts.cs

---
+++ FootballRanking/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballRanking
+{
+    class Team
+    {
+        private string name;
+        private int points;
+
+        public Team(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+    }
---
+++ FootballRanking/TeamFacts.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FootballRanking
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
