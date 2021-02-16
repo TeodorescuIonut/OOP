7365cb70bb31868bd6bd45774ae2824713c73e44
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-02-16 13:30:43 +0200

    formatted code for Sequence and Choice

--- JSON classes/Range/Choice.cs
+++ JSON classes/Range/Choice.cs
@@ -4,20 +4,20 @@ using System.Text;
 
 namespace JSONclasses
 {
-    class Choice: IPattern
+    class Choice : IPattern
     {
         private readonly IPattern[] patterns;
 
         public Choice(params IPattern[] patterns)
         {
             this.patterns = patterns;
-    }
+        }
 
         IMatch IPattern.Match(string text)
         {
             IMatch match = new FailedMatch(text);
 
-            foreach(var pattern in patterns)
+            foreach (var pattern in patterns)
             {
                 match = pattern.Match(match.RemainingText());
                 if (match.Success())
--- JSON classes/Range/Sequence.cs
+++ JSON classes/Range/Sequence.cs
@@ -10,7 +10,7 @@ namespace JSONclasses
         public Sequence(params IPattern[] patterns)
         {
             this.patterns = patterns;
-    }
+        }
         public IMatch Match(string text)
         {
             IMatch match = new FailedMatch(text);
@@ -19,12 +19,12 @@ namespace JSONclasses
                 match = pattern.Match(match.RemainingText());
                 if (!match.Success())
                 {
-                    
+
                     return new FailedMatch(text);
-                }               
+                }
             }
             return match;
-            
+
         }
     }
 }
