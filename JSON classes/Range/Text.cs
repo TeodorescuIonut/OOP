6a7c83c9738528be42d1536c9b02c7233240a42b
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-02-17 14:16:14 +0200

    added class Text and TextFacts

---
+++ JSON classes/Range/Text.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONclasses
+{
+    class Text : IPattern
+    {
+        private readonly string prefix;
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+        public IMatch Match(string text)
+        {
+            IMatch match = new FailedMatch(text);
+            string remainedText = text;
+            if (string.IsNullOrEmpty(text))
+            {
+                return match;
+            }
+
+            foreach (char c in text)
+            {
+                if (!prefix.Contains(c))
+                {
+                    return match;
+                }
+                remainedText = remainedText.Substring(1);
+                match = new SuccessMatch(remainedText);
+
+            }
+            return match;
+        }
+    }
+}
