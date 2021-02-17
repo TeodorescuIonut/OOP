dacfb5714f7ee320b11552d02b2bf3d754fcc7e2
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-02-17 19:30:35 +0200

    refactor class Text

--- JSON classes/Range/Text.cs
+++ JSON classes/Range/Text.cs
@@ -13,7 +13,7 @@ namespace JSONclasses
         }
         public IMatch Match(string text)
         {
-            return !string.IsNullOrEmpty(text) && text.Contains(prefix) ?
+            return !string.IsNullOrEmpty(text) && text.StartsWith(prefix, 0) ?
                 new SuccessMatch(text.Substring(prefix.Length)) :
                 (IMatch)new FailedMatch(text);
         }
