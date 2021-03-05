e9f856e5d6ad62b1479a89215de3346a7c0c8d41
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-03-05 09:50:39 +0200

    corrected class List

---
+++ JSON classes/Range/List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONclasses
+{
+    class List : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Many(new Choice(element,new Sequence(separator, element)));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
