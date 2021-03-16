83f09bebc0cfe819d740cde33b1795abba19812d
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-03-16 22:24:39 +0200

    added class Value and tests

---
+++ JSON classes/Range/Value.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONclasses
+{
+     class Value : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Value()
+        {
+            var stringText = new String();
+            var number = new Number();
+            var value = new Choice(
+                stringText,
+                number,
+                new Text("true"),
+                new Text("false"),
+                new Text("null")
+            );
+            var ws = new Choice(
+                     new Character(Convert.ToChar(Convert.ToUInt32("0020", 16))),
+                     new Character(Convert.ToChar(Convert.ToUInt32("000A", 16))),
+                     new Character(Convert.ToChar(Convert.ToUInt32("000D", 16))),
+                     new Character(Convert.ToChar(Convert.ToUInt32("0009", 16))));
+            var element = new Sequence(ws, value, ws);
+            var elements = new List(element, new Character(','));
+            var member = new Sequence(ws, stringText, ws, new Character(':'), element);
+            var members = new List(member, new Character(','));
+            var array = new Sequence(
+                new Character('['),
+                new Optional(ws), 
+                elements, 
+                new Character('}'));
+            var objects = new Sequence(
+                new Character('{'),
+                new Optional(ws),
+                members,
+                new Character(']'));
+            value.Add(array);
+            value.Add(objects);
+            pattern = value;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ JSON classes/Range/ValueFacts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Xunit;
+
+namespace JSONclasses
+{
+    public class ValueFacts
+    {
+        [Fact]
+        public void CheckIfString()
+        {
+            var value = new Value();
+            string text = Quoted("abc");
+            Assert.Equal("", value.Match(text).RemainingText());
+            Assert.True(value.Match(text).Success());
+        }
+        [Fact]
+        public void CheckIfNumber()
+        {
+            var value = new Value();
+            const string text = "7121";
+            Assert.Equal("", value.Match(text).RemainingText());
+            Assert.True(value.Match(text).Success());
+        }
+        [Fact]
+        public void CheckIfNull()
+        {
+            var value = new Value();
+            const string text = null;
+            Assert.Null(value.Match(text).RemainingText());
+            Assert.False(value.Match(text).Success());
+        }
+        [Fact]
+        public void CheckIfFalse()
+        {
+            var value = new Value();
+            const string text = "false";
+            Assert.Equal("",value.Match(text).RemainingText());
+            Assert.True(value.Match(text).Success());
+        }
+
+        [Fact]
+        public void CheckIfTrue()
+        {
+            var value = new Value();
+            const string text = "true";
+            Assert.Equal("", value.Match(text).RemainingText());
+            Assert.True(value.Match(text).Success());
+        }
+        public static string Quoted(string text)
+        => $"\"{text}\"";
+    }
+}
