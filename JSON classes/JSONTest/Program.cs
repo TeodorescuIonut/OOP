0b524f17cc50538282f6d554ca15c3c4e044388a
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-03-19 16:53:24 +0200

    updated JSONTest

--- JSON classes/JSONTest/Program.cs
+++ JSON classes/JSONTest/Program.cs
@@ -7,9 +7,12 @@ namespace JSONTest
     {
         static void Main()
         {
-            string text = System.IO.File.ReadAllText(@"C:\Users\Ionut\Documents\GitHub\OOP\JSON classes\WriteText.txt");
+            string text = Console.ReadLine();
+            text = System.IO.File.ReadAllText(@text);
             var value = new Value();
-            Console.WriteLine(value.Match(text).Success() ? text + " is a valid JSON format" : text + " is not  a valid JSON format");
+            Console.WriteLine(value.Match(text).Success() && value.Match(text).RemainingText() == ""
+                                ? text + " is a valid JSON format"
+                                : text + " is not  a valid JSON format");       
         }
        
     }
