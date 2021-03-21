0be9120ebbbd44559dc5a10a489da32875c09097
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-03-21 13:00:48 +0200

    Added project console JSONTest

---
+++ JSONclasses/JSONTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace JSONclasses
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            string path = args[0];
+            if (File.Exists(path))
+            {
+                string fileName = Path.GetFileName(path);
+                string text = File.ReadAllText(path);
+                var value = new Value();
+                Console.WriteLine(value.Match(text).Success() && value.Match(text).RemainingText()?.Length == 0
+                                 ? fileName + " contains valid JSON format"
+                                 : fileName + " doesnt contain valid JSON format");
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine("Path doesn't exist. Please enter a valid path.");
+            }
+        }
+    }
+}
