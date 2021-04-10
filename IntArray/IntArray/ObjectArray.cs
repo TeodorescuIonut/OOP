29a9da88940c4a9a63f9b1d51e9182c6732547f1
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-04-10 19:49:58 +0300

    Added new Class ObjectArray and tests

---
+++ IntArray/IntArray/ObjectArray.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public class ObjectArray
+    {
+        private object[] objects;
+
+        public ObjectArray()
+        {
+            const int size = 4;
+            objects = new object[size];
+        }
+
+        public int Count { get; private set; }
+
+        public object this[int index]
+        {
+            get => objects[index];
+            set => objects[index] = value;
+        }
+
+        public virtual void Add(object element)
+        {
+            EnsureCapacity();
+            objects[Count] = element;
+            Count++;
+        }
+
+        public bool Contains(object element)
+        {
+            return IndexOf(element) != -1;
+        }
+
+        public int IndexOf(object element)
+        {
+            return Array.IndexOf(objects, element, 0, Count);
+        }
+
+        public void Insert(int index, object element)
+        {
+            EnsureCapacity();
+            ShiftRight(index);
+            objects[index] = element;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            objects = Array.Empty<object>();
+            Count = 0;
+        }
+
+        public void Remove(object element)
+        {
+            int index = IndexOf(element);
+            if (index == -1)
+            {
+                return;
+            }
+
+            RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeft(index);
+            Count--;
+            Array.Resize(ref objects, Count);
+        }
+
+        public void ShiftLeft(int index)
+        {
+            for (int j = index; j < Count - 1; j++)
+            {
+                objects[j] = objects[j + 1];
+            }
+        }
+
+        public void ShiftRight(int index)
+        {
+            for (int j = Count; j > index; j--)
+            {
+                objects[j] = objects[j - 1];
+            }
+        }
+
+        public void EnsureCapacity()
+        {
+            if (Count < objects.Length)
+            {
+                return;
+            }
+
+            const int sizeDouble = 2;
+            Array.Resize(ref objects, objects.Length * sizeDouble);
+        }
+}
+}
---
+++ IntArray/IntArrayFacts/ObjectArrayFactscs.cs
@@ -0,0 +1,57 @@
+﻿using Arrays;
+using System;
+using Xunit;
+
+
+namespace IntArrayFacts
+{
+     public class ObjectArrayFactscs
+    {
+        [Fact]
+        public void CheckIfIntegerCanBeAdded()
+        {
+            var integerArr = new ObjectArray();
+            integerArr.Add(5);
+            integerArr.Add(4);
+            integerArr.Add(1);
+            integerArr.Add(4);
+            integerArr.Add(32);
+            integerArr.Add(18);
+            integerArr.Add(0);
+            Assert.Equal(1, integerArr[2]);
+        }
+        [Fact]
+        public void CheckIfContainsSpecificNumber()
+        {
+            var myArray = new ObjectArray();
+            int numberOne = 4;
+            string numberTwo = "7";
+            myArray.Add(numberOne);
+            myArray.Add(numberTwo);
+            string numberThree = "7";
+            Assert.True(myArray.Contains(numberThree));
+        }
+
+        [Fact]
+        public void CheckIfStringtCanBeAdded()
+        {
+            var stringArr = new ObjectArray();
+            stringArr.Add("Jhon");
+            stringArr.Add("Mike");
+            Assert.Equal("Mike", stringArr[1]);
+        }
+
+        [Fact]
+        public void InsertElementAtIndexGiven()
+        {
+            var myArray = new ObjectArray();
+            int numberOne = 4;
+            double numberTwo = 12.3;
+            string numberThree = "12";
+            myArray.Add(numberOne);
+            myArray.Add(numberTwo);
+            myArray.Insert(1, numberThree);
+            Assert.Equal(1, myArray.IndexOf(numberThree));
+        }
+    }
+}
