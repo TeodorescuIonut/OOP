21e988945e3f57171c414597ff91c463b8b0ed15
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-09-20 15:16:38 +0300

    corrected GetIntegerFromString method

--- StringTest/StringTestFacts.cs
+++ StringTest/StringTestFacts.cs
@@ -26,14 +26,30 @@ namespace StringTest
         [Fact]
         public static void ReturnFromStringAnInteger()
         {
-            const string numberAsString = "1234";
+            const string numberAsString = "-1234";
             var numbersAsInt = GetIntegerFromString(numberAsString);
-            Assert.Equal(1234, numbersAsInt);
+            Assert.Equal(-1234, numbersAsInt);
+        }
+
+        [Fact]
+        public static void ReturnFromStringCharWithMoreOCurrance()
+        {
+            const string word = "abstwqeeeertwesl";
+            var character = word.GroupBy(x => x).Where(x => x.Key > 1);
         }
 
         private static int GetIntegerFromString(string word)
         {
-            return word.Select(x => x - '0').Aggregate((result, x) => result * 10 + x);
+            if (word.StartsWith('-') && word[1..].All(x => char.IsDigit(x)))
+            {
+                return word[1..].Select(x => x - '0').Aggregate((result, x) => result * 10 + x) * -1;
+            }
+            else if (word.All(x => char.IsDigit(x)))
+            {
+                return word.Select(x => x - '0').Aggregate((result, x) => result * 10 + x);
+            }
+
+            throw new ArgumentException("Please enter a valid number", nameof(word));
         }
 
         private static (int vocalsNo, int consNo) GetNoOfVowelsAndConsonants(this string word)
