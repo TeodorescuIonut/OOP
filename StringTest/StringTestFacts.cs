5696b63811632c9e014084eadd2154be1dd2bbae
TeodorescuIonut <teodorescuionutz@gmail.com>
2021-10-11 11:54:47 +0300

    added method GetSumCombinations

--- StringTest/StringTestFacts.cs
+++ StringTest/StringTestFacts.cs
@@ -58,6 +58,51 @@ namespace StringTest
             Assert.Equal(3, subArrays[3][0]);
         }
 
+        [Fact]
+        public static void REturnAllSumCombinationss()
+        {
+            const int numberOfElem = 6;
+            const int k = 3;
+            var subArrays = GetSumCombinations(numberOfElem, k);
+            Assert.Equal("+1-2-3-4+5+6 = 3", subArrays[0]);
+        }
+
+        private static List<string> GetSumCombinations(int elemNo, int targetSum)
+        {
+            string[] elements = new[] { "" };
+            for (int i = 0; i < elemNo; i++)
+            {
+                int size = elements.Length * 2;
+                Array.Resize(ref elements, size);
+                Array.Copy(elements, 0, elements, elements.Length / 2, elements.Length - elements.Length / 2);
+                elements = elements.Select((x, index) => index < elements.Length / 2 ? x + "+" + (i + 1) : x + "-" + (i + 1)).ToArray();
+            }
+
+            List<string> results = new List<string>();
+            foreach (var str in elements)
+            {
+                int sum = 0;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[i] == '-')
+                    {
+                        sum -= str[i + 1] - '0';
+                    }
+                    else if (str[i] == '+')
+                    {
+                        sum += str[i + 1] - '0';
+                    }
+                }
+
+                if (sum == targetSum)
+                {
+                    results.Add(str + " = " + targetSum);
+                }
+            }
+
+            return results;
+        }
+
         private static int[][] GetSubArraysCombinations(int[] numArray, int targetSum)
         {
             return numArray.SelectMany((x, index) => numArray.Skip(index).Select((i, j) => numArray[index..i])).Where(x => x.Sum() <= targetSum).ToArray();
